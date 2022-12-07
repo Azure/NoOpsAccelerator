416a35813d0d7122e85a774aa50d759bc7fe324e
wtomaz808 <42211687+wtomaz808@users.noreply.github.com>
2022-12-07 02:58:01 +0000

    added code from locahost

--- src/bicep/overlays/management-services/vm-tak-server/scripts/installscript.sh
+++ src/bicep/overlays/management-services/vm-tak-server/scripts/installscript.sh
@@ -8,29 +8,35 @@ RPM_DBsource="https://noopsblobstorage.blob.core.usgovcloudapi.net/anoatak/postg
 # new from takimage-main
 script_home=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
-
 echo 'cd ~' >> /root/.bashrc
 
-# Install postgres
-yum -y install postgresql10-server unzip
+# Install postgresql
+curl -fskSL -o "${RPM_source}/postgresql14-14.6-1PGDG.rhel7.x86_64.rpm" "${script_home}/takdb.rpm"
 
-curl -fskSL -o "${script_home}/takserver.rpm" "${script_home}"
+[[ ! -f "${script_home}/takdb.rpm" ]] && exit 1
 
-# [[ ! -f "${script_home}/takserver.rpm" ]] && exit 1
+yum -y localinstall "${script_home}/takdb.rpm" --nogpgcheck
 
-# yum -y localinstall "${script_home}/takserver.rpm" --nogpgcheck
-# /bin/bash /opt/tak/db-utils/takserver-setup-db.sh
+# Install Tak server
+curl -fskSL -o "${RPM_source}/takserver-4.7-RELEASE20.noarch.rpm" "${script_home}/takserver.rpm"
 
-# grep -m1 'keystorePass' /opt/tak/CoreConfig.example.xml | awk -F\" '{print $6}' > "${script_home}/.jks"
-# echo "ftpuser:ftpuser$(seq -s. 4 | tr -d '.')" > "${script_home}/.ftp"
-# echo "$(openssl $(openssl / 2>&1 | head -9 | tail -1 | awk '{print $3}') --help 2>&1 | head -1 | awk '{print substr($2,1,4)}' | sed 's/.*/\u&/')4marti$(seq -s. 4 | tr -d '.')"'!' > "${script_home}/.marti"
+[[ ! -f "${script_home}/takserver.rpm" ]] && exit 1
 
-# sed -i "s/#DBP/$(grep connection /opt/tak/CoreConfig.example.xml | awk -F\" '{print $6}')/g" "${script_home}/CoreConfig.xml"
-# sed -i "s/#SS/$(hostname -A | awk -F. '{print $2}')/g" "${script_home}/CoreConfig.xml"
-# sed -i "s/#JKS/$(cat ${script_home}/.jks)/g" "${script_home}/CoreConfig.xml"
+yum -y localinstall "${script_home}/takserver.rpm" --nogpgcheck
 
-# rm -f ${script_home}/takserver.rpm
-# rm -rf ${script_home}/${project}-main
+# Configure Tak Server
+/bin/bash /opt/tak/db-utils/takserver-setup-db.sh
 
-# yum -y update
+grep -m1 'keystorePass' /opt/tak/CoreConfig.example.xml | awk -F\" '{print $6}' > "${script_home}/.jks"
+echo "ftpuser:ftpuser$(seq -s. 4 | tr -d '.')" > "${script_home}/.ftp"
+echo "$(openssl $(openssl / 2>&1 | head -9 | tail -1 | awk '{print $3}') --help 2>&1 | head -1 | awk '{print substr($2,1,4)}' | sed 's/.*/\u&/')4marti$(seq -s. 4 | tr -d '.')"'!' > "${script_home}/.marti"
+
+sed -i "s/#DBP/$(grep connection /opt/tak/CoreConfig.example.xml | awk -F\" '{print $6}')/g" "${script_home}/CoreConfig.xml"
+sed -i "s/#SS/$(hostname -A | awk -F. '{print $2}')/g" "${script_home}/CoreConfig.xml"
+sed -i "s/#JKS/$(cat ${script_home}/.jks)/g" "${script_home}/CoreConfig.xml"
+
+rm -f ${script_home}/takserver.rpm
+rm -rf ${script_home}/${project}-main
+
+yum -y update
 ###
