635af4684548dfa2907e91b69a3fb6ba1ccd1cb7
wtomaz808 <42211687+wtomaz808@users.noreply.github.com>
2022-12-07 00:38:34 +0000

    added new global_vars file
    update to existing var file

---
+++ src/bicep/overlays/management-services/vm-tak-server/scripts/global_vars.sh
@@ -0,0 +1,7 @@
+project="TakImage"
+RPM_TAKsource="https://noopsblobstorage.blob.core.usgovcloudapi.net/anoatak/takserver-4.7-RELEASE20.noarch.rpm?sv=2021-08-06&st=2022-11-29T03%3A07%3A13Z&se=2022-11-30T03%3A07%3A13Z&sr=b&sp=rw&sig=9L8sqD4iiUa7VxLrq7t4D5sulmRIPoebDhq2sVpqVkg%3D"
+RPM_DBsource="https://noopsblobstorage.blob.core.usgovcloudapi.net/anoatak/postgresql14-14.6-1PGDG.rhel7.x86_64.rpm?sv=2021-08-06&st=2022-11-29T03%3A09%3A23Z&se=2022-11-30T03%3A09%3A23Z&sr=b&sp=rw&sig=QzVBGcmI%2BNvvP%2FzpxuuBUfBISF%2B9Dsu9DBruxHZ%2FAns%3D"
+# new from takimage-main
+script_home=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+# old from orig repo
+# script_home="~/script/"
