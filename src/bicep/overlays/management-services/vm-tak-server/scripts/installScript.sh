902649070de5d2f6516a34fc72fc7c3790e71db3
wtomaz808 <42211687+wtomaz808@users.noreply.github.com>
2022-12-07 00:55:09 +0000

    updated tak & postgres sas urls

--- src/bicep/overlays/management-services/vm-tak-server/scripts/installScript.sh
+++ src/bicep/overlays/management-services/vm-tak-server/scripts/installScript.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo "Running script to begin the install process for TAK Server, it will take a while so please be patient."
 
-# source global_vars.sh
-# project="TakImage"
+# source global_vars.sh add var here since global_var can not be read from vm extention 
+project="TakImage"
+RPM_TAKsource="https://noopsblobstorage.blob.core.usgovcloudapi.net/anoatak/takserver-4.7-RELEASE20.noarch.rpm?sp=r&st=2022-12-07T00:51:22Z&se=2022-12-07T08:51:22Z&spr=https&sv=2021-06-08&sr=b&sig=aQB1tSL2BPLqeKz%2BKysdovJdQ8BpmIjOb9nN3vdnnqw%3D"
+RPM_DBsource="https://noopsblobstorage.blob.core.usgovcloudapi.net/anoatak/postgresql14-14.6-1PGDG.rhel7.x86_64.rpm?sp=r&st=2022-12-07T00:53:11Z&se=2022-12-07T08:53:11Z&spr=https&sv=2021-06-08&sr=b&sig=TQlHvMdyzbP1XBjVM6u%2FwBOJVnkfqoYn%2BC4VA1scWJc%3D"
+# new from takimage-main
+script_home=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
 
 # echo 'cd ~' >> /root/.bashrc
 
