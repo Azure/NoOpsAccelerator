1958f916870d0ffc89f0d1222cb237cbab5fb11e
wtomaz808 <42211687+wtomaz808@users.noreply.github.com>
2023-01-06 21:25:39 +0000

    Update installscript.sh
    updated SAS token and enable yum update -y  line 37 & 49

--- src/bicep/overlays/management-services/vm-tak-server/scripts/installscript.sh
+++ src/bicep/overlays/management-services/vm-tak-server/scripts/installscript.sh
@@ -10,7 +10,7 @@ AZ_BLOB_URL="https://noopsblobstorage.blob.core.usgovcloudapi.net"
 AZ_BLOB_CONTAINER="anoatak"
 AZ_BLOB_SOURCEDIR="rpm_source_files"
 AZ_BLOB_TARGET="${AZ_BLOB_URL}/${AZ_BLOB_CONTAINER}/${AZ_BLOB_SOURCEDIR}/"
-AZ_SAS_TOKEN="sp=r&st=2022-12-20T18:13:17Z&se=2022-12-30T02:13:17Z&spr=https&sv=2021-06-08&sr=c&sig=SPPo8fOZ7RW1%2FpgH4ypzpebB%2BKbPN%2Bgnws4uerk2fBM%3D"
+AZ_SAS_TOKEN="sp=r&st=2023-01-06T21:23:07Z&se=2023-03-31T05:23:07Z&spr=https&sv=2021-06-08&sr=c&sig=4vkd7dvVvr5czH2BrrOKAqaofRGmgqUztxnaS6IscEM%3D"
 
 script_home=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
@@ -34,8 +34,7 @@ echo "Running script Line 25 epel-release install."
 sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 echo "Running script Line 34 Install postgres yum repository"
 
-# yum update
-# sudo yum update -y
+sudo yum update -y
 echo "Running line 38 yum update"
 
 # Install Tak server
@@ -46,6 +45,8 @@ curl "${AZ_BLOB_TARGET}takserver-4.7-RELEASE20.noarch.rpm?${AZ_SAS_TOKEN}" --out
 sudo chmod +x "takserver.rpm"
 sudo yum -y localinstall "${script_home}/takserver.rpm" --nogpgcheck
 echo "Running line 46 install takserver"
+
+sudo yum update -y
 
 # configure tak server
 echo "Running TAK Server config"
